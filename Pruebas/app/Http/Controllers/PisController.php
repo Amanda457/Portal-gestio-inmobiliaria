d40072ab5903f3f6f01f2c6d4531af90bd9f4039
Amanda457 <mandy96lomi@gmail.com>
2024-06-03 13:23:33 +0200

    model Pis inicial

---
+++ Pruebas/app/Http/Controllers/PisController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Pis;
+use App\Http\Requests\StorePisRequest;
+use App\Http\Requests\UpdatePisRequest;
+
+class PisController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StorePisRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Pis $pis)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Pis $pis)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdatePisRequest $request, Pis $pis)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Pis $pis)
+    {
+        //
+    }
+}
---
+++ Pruebas/database/migrations/2024_06_03_111103_create_pis_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pis', function (Blueprint $table) {
+            $table->id();
+            $table->string('nom_referencia');
+            $table->string('direccio');
+            $table->integer('m2');
+            $table->integer('habitacions');
+            $table->integer('lavabos');
+            $table->integer('preu');
+            $table->string('tipus_cuina');
+            $table->text('descripcio');
+            $table->boolean('ascensor');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pis');
+    }
+};
