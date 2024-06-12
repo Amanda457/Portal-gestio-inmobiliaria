714fd2bec5a46320621c01fa150103563ad69617
Amanda457 <mandy96lomi@gmail.com>
2024-06-12 11:36:20 +0200

    creació vista index i crear estructura de la taula

---
+++ Pruebas/resources/views/clients/index.blade.php
@@ -0,0 +1,41 @@
+<!-- resources/views/clients/index.blade.php -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index Clients</title>
+</head>
+<body>
+    
+    <table class="table-auto w-full">
+        <thead>
+            <tr>
+                <th class="px-4 py-2 text-gray-900 dark:text-white text-center">ID</th>
+                <th class="px-4 py-2 text-gray-900 dark:text-white text-center">Nom</th>
+                <th class="px-4 py-2 text-gray-900 dark:text-white text-center">Cognom</th>
+                <th class="px-4 py-2 text-gray-900 dark:text-white text-center">Telèfon</th>
+                <th class="px-4 py-2 text-gray-900 dark:text-white text-center">Data de naixement</th>
+                <th class="px-4 py-2 text-gray-900 dark:text-white text-center">Correu electrònic</th>
+                <th class="px-4 py-2 text-gray-900 dark:text-white text-center">Accions</th>
+
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($clients as $client)
+            <tr>
+                <td class="border px-4 py-2 text-gray-900 dark:text-white text-center">{{ $client->id }}</td>
+                <td class="border px-4 py-2 text-gray-900 dark:text-white text-center">{{ $client->nom }}</td>
+                <td class="border px-4 py-2 text-gray-900 dark:text-white text-center">{{ $client->cognom }}</td>
+                <td class="border px-4 py-2 text-gray-900 dark:text-white text-center">{{ $client->telefon}}</td>
+                <td class="border px-4 py-2 text-gray-900 dark:text-white text-center">{{ $client->data_naixement }}</td>
+                <td class="border px-4 py-2 text-gray-900 dark:text-white text-center">{{ $client->email }}</td>
+                </tr>
+            @endforeach
+                
+         </tbody>
+    </table>
+    
+</body>
+</html>
+    
