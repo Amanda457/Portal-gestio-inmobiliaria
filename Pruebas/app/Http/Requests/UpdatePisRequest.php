5ebc2f1a6c3ddae13a482a3e9f27b3f3d2d0b8bc
Amanda457 <mandy96lomi@gmail.com>
2024-06-25 13:01:12 +0200

    validacions poblacio

--- Pruebas/app/Http/Requests/UpdatePisRequest.php
+++ Pruebas/app/Http/Requests/UpdatePisRequest.php
@@ -25,6 +25,7 @@ class UpdatePisRequest extends FormRequest
             'nom_referencia' => 'required|string|min:3|max:50',
             'tipus' => ['required', 'string', 'in:Pis,Casa'],
             'direccio' => 'required|string|min:3|max:70',
+            'poblacio' => 'required|string|min:3|max:30',
             'm2' => 'required|integer|between:30,999',
             'habitacions' => 'required|integer|between:1,12',
             'lavabos' => 'required|integer|between:1,12',
